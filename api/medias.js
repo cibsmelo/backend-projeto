32eef0d861838699981bd0b6f49757298a685e58
CIBELY SOUSA MELO <00001098178567sp@aluno.educacao.sp.gov.br>
2026-05-04 13:41:43 -0300

    feat: adicionado novo arquivo media.js .

---
+++ api/medias.js
@@ -0,0 +1,21 @@
+const medias = [
+    { id: 1, type: "movie", title: "Inception" },
+    { id: 2, type: "tv", title: "Breaking Bad" },
+    { id: 3, type: "movie", title: "The Matrix" },
+    { id: 4, type: "tv", title: "Game of Thrones" }
+  ];
+  
+  module.exports = (req, res) => {
+    if (req.method === "GET") {
+      return res.status(200).json({ medias });
+    } else if (req.method === "POST") {
+      const newMedia = req.body;
+      return res.status(201).json({
+        message: "Media created",
+        media: newMedia,
+        totalMedias: [...medias, newMedia].length,
+        allMedias: [...medias, newMedia]
+      });
+    }
+  };
+  
